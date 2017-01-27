a332af4a3f7071139bd02ac265c180e7e7f0ae4b
rudiaj <aj.rudi@gmail.com>
2017-01-27 16:06:47 +0100

    cleaned data, added open hours and open status, tabular presentation of meals

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,188 +9,113 @@
   function MainController($timeout, webDevTec, toastr) {
     var vm = this;
 
-    vm.restaurants =[  
-    {  
-      "id":1,
-      "name":"McDonalds",
-      "username":"Bret",
-      "email":"Sincere@april.biz",
-      "image":"https://unsplash.it/400/200/?random",
-      "offers":[  
-      {  
-        "name":"Grilled Salmon",
-        "category":"fish",
-        "price":14
+    vm.restaurants =[{
+      "id": 1,
+      "name": "McDonalds",
+      "image": "assets/images/food_image_1.jpg",
+      "open_status": {
+        "open_now": true,
+        "open_hours": "08:00 - 16:00"
       },
-      {  
-        "name":"Steak",
-        "category":"meat",
-        "price":20
+      "offers": [{
+        "name": "Grilled Salmon",
+        "category": "fish",
+        "price": 14
+      }, {
+        "name": "Steak",
+        "category": "meat",
+        "price": 20
+      }],
+      "address": {
+        "street": "Kulas Light",
+        "city": "Gwenborough"
       }
-      ],
-      "address":{  
-       "street":"Kulas Light",
-       "suite":"Apt. 556",
-       "city":"Gwenborough",
-       "zipcode":"92998-3874",
-       "geo":{  
-        "lat":"-37.3159",
-        "lng":"81.1496"
+    }, {
+      "id": 2,
+      "name": "KFC",
+      "image": "assets/images/food_image_2.jpg",
+      "open_status": {
+        "open_now": true,
+        "open_hours": "08:00 - 16:00"
+      },
+      "offers": [{
+        "name": "Pasta Carbonara",
+        "category": "pasta",
+        "price": 8
+      }, {
+        "name": " Griller Chicken",
+        "category": "meat",
+        "price": 20
+      }],
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh"
       }
-    },
-    "phone":"1-770-736-8031 x56442",
-    "website":"hildegard.org",
-    "company":{  
-     "name":"Romaguera-Crona",
-     "catchPhrase":"Multi-layered client-server neural-net",
-     "bs":"harness real-time e-markets"
-   }
- },
- {  
-  "id":2,
-  "name":"KFC",
-  "username":"Antonette",
-  "email":"Shanna@melissa.tv",
-  "image":"https://unsplash.it/400/200/?random",
-  "offers":[  
-  {  
-    "name":"Pasta Carbonara",
-    "category":"pasta",
-    "price":8
-  },
-  {  
-    "name":" Griller Chicken",
-    "category":"meat",
-    "price":20
-  }
-  ],
-  "address":{  
-   "street":"Victor Plains",
-   "suite":"Suite 879",
-   "city":"Wisokyburgh",
-   "zipcode":"90566-7771",
-   "geo":{  
-    "lat":"-43.9509",
-    "lng":"-34.4618"
-  }
-},
-"phone":"010-692-6593 x09125",
-"website":"anastasia.net",
-"company":{  
- "name":"Deckow-Crist",
- "catchPhrase":"Proactive didactic contingency",
- "bs":"synergize scalable supply-chains"
-}
-},
-{  
-  "id":3,
-  "name":"Domionos",
-  "username":"Samantha",
-  "email":"Nathan@yesenia.net",
-  "image":"https://unsplash.it/400/200/?random",
-  "offers":[  
-  {  
-    "name":"Spicy Tuna Roll",
-    "category":"fish",
-    "price":10
-  },
-  {  
-    "name":"Steak",
-    "category":"meat",
-    "price":20
-  }
-  ],
-  "address":{  
-   "street":"Douglas Extension",
-   "suite":"Suite 847",
-   "city":"McKenziehaven",
-   "zipcode":"59590-4157",
-   "geo":{  
-    "lat":"-68.6102",
-    "lng":"-47.0653"
-  }
-},
-"phone":"1-463-123-4447",
-"website":"ramiro.info",
-"company":{  
- "name":"Romaguera-Jacobson",
- "catchPhrase":"Face to face bifurcated interface",
- "bs":"e-enable strategic applications"
-}
-},
-{  
-  "id":4,
-  "name":"Burger King",
-  "username":"Karianne",
-  "email":"Julianne.OConner@kory.org",
-  "image":"https://unsplash.it/400/200/?random",
-  "offers":[  
-  {  
-    "name":"Hamburger",
-    "category":"meat",
-    "price":14
-  },
-  {  
-    "name":" Cheesburger",
-    "category":"meat",
-    "price":20
-  }
-  ],
-  "address":{  
-   "street":"Hoeger Mall",
-   "suite":"Apt. 692",
-   "city":"South Elvis",
-   "zipcode":"53919-4257",
-   "geo":{  
-    "lat":"29.4572",
-    "lng":"-164.2990"
-  }
-},
-"phone":"493-170-9623 x156",
-"website":"kale.biz",
-"company":{  
- "name":"Robel-Corkery",
- "catchPhrase":"Multi-tiered zero tolerance productivity",
- "bs":"transition cutting-edge web services"
-}
-},
-{  
-  "id":5,
-  "name":"Caribic Pizza",
-  "username":"Kamren",
-  "email":"Lucio_Hettinger@annie.ca",
-  "image":"https://unsplash.it/400/200/?random",
-  "offers":[  
-  {  
-    "name":"Grilled Salmon",
-    "category":"fish",
-    "price":14
-  },
-  {  
-    "name":"Salad",
-    "category":"vegeterian",
-    "price":5
-  }
-  ],
-  "address":{  
-   "street":"Skiles Walks",
-   "suite":"Suite 351",
-   "city":"Roscoeview",
-   "zipcode":"33263",
-   "geo":{  
-    "lat":"-31.8129",
-    "lng":"62.5342"
-  }
-},
-"phone":"(254)954-1289",
-"website":"demarco.info",
-"company":{  
- "name":"Keebler LLC",
- "catchPhrase":"User-centric fault-tolerant solution",
- "bs":"revolutionize end-to-end systems"
-}
-}
-]
+    }, {
+      "id": 3,
+      "name": "Domionos",
+      "image": "assets/images/food_image_3.jpg",
+      "open_status": {
+        "open_now": true,
+        "open_hours": "08:00 - 16:00"
+      },
+      "offers": [{
+        "name": "Spicy Tuna Roll",
+        "category": "fish",
+        "price": 10
+      }, {
+        "name": "Steak",
+        "category": "meat",
+        "price": 20
+      }],
+      "address": {
+        "street": "Douglas Extension",
+        "city": "McKenziehaven"
+      }
+    }, {
+      "id": 4,
+      "name": "Burger King",
+      "image": "assets/images/food_image_4.jpg",
+      "open_status": {
+        "open_now": false,
+        "open_hours": "08:00 - 16:00"
+      },
+      "offers": [{
+        "name": "Hamburger",
+        "category": "meat",
+        "price": 14
+      }, {
+        "name": " Cheesburger",
+        "category": "meat",
+        "price": 20
+      }],
+      "address": {
+        "street": "Hoeger Mall",
+        "city": "South Elvis"
+      }
+    }, {
+      "id": 5,
+      "name": "Caribic Pizza",
+      "image": "assets/images/food_image_5.jpg",
+      "open_status": {
+        "open_now": false,
+        "open_hours": "08:00 - 16:00"
+      },
+      "offers": [{
+        "name": "Grilled Salmon",
+        "category": "fish",
+        "price": 14
+      }, {
+        "name": "Salad",
+        "category": "vegeterian",
+        "price": 5
+      }],
+      "address": {
+        "street": "Skiles Walks",
+        "city": "Roscoeview"
+      }
+    }]
 
 
 
